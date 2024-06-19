1f73ada124d2ef1e651e2ce5be9d7f701fe50e4e
NDROID228 <djunsonik@gmail.com>
2024-06-19 17:42:43 +0300

    asdfg

--- utils/analyzeImage.js
+++ utils/analyzeImage.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const fs = require("fs");
-const FileReader = require('filereader')
+const FileReader = require("filereader");
 const { OpenAI } = require("openai");
 
 async function convertToBase64(file) {
@@ -12,38 +12,39 @@ async function convertToBase64(file) {
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-
-async function analyzeImage(imageFile) {
+async function askVision (image_url) {
   let message = undefined;
 
-  const reader = new FileReader()
-  reader.readAsDataURL(imageFile)
-  reader.addEventListener('load', async () => {
-    try {
-      const response = await openai.chat.completions.create({
-        model: "gpt-4-vision-preview",
-        messages: [
-          {
-            role: "user",
-            content: [
-              { type: "text", text: "Що на цьому зображені?" },
-              {
-                type: "image_url",
-                image_url: reader.result,
-              },
-            ],
-          },
-        ],
-        max_tokens: 20,
-      });
-      console.log(response.choices[0]);
-      message = response.choices[0].message.content;
-    } catch (error) {
-      console.log(error);
-    }
-    reader.removeEventListener('load')
-  })
-  
+  try {
+    const response = await openai.chat.completions.create({
+      model: "gpt-4-vision-preview",
+      messages: [
+        {
+          role: "user",
+          content: [
+            { type: "text", text: "Що на цьому зображені?" },
+            {
+              type: "image_url",
+              image_url: image_url,
+            },
+          ],
+        },
+      ],
+      max_tokens: 20,
+    });
+    console.log(response.choices[0]);
+    message = response.choices[0].message.content;
+  } catch (error) {
+    console.log(error);
+  }
+  return message;
+};
+async function analyzeImage(imageFile) {
+
+  const reader = new FileReader();
+  reader.readAsDataURL(imageFile);
+  reader.addEventListener("load", askVision(reader.result));
+  reader.removeEventListener(askVision);
   return message;
 }
 
