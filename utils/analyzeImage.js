b2e8b0792eab299a9523c51e63cbb064f0e7dba1
NDROID228 <djunsonik@gmail.com>
2024-06-19 17:16:07 +0300

    asdfghjk

--- utils/analyzeImage.js
+++ utils/analyzeImage.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const fs = require("fs");
+const FileReader = require('filereader')
 const { OpenAI } = require("openai");
 
 async function convertToBase64(file) {
@@ -12,32 +13,37 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-async function analyzeImage(image_url) {
-
+async function analyzeImage(imageFile) {
   let message = undefined;
-  try {
-    const response = await openai.chat.completions.create({
-      model: "gpt-4-vision-preview",
-      messages: [
-        {
-          role: "user",
-          content: [
-            { type: "text", text: "Що на цьому зображені?" },
-            {
-              type: "image_url",
-              image_url: image_url,
-            },
-          ],
-        },
-      ],
-      max_tokens: 20,
-    });
-    console.log(response.choices[0]);
-    message = response.choices[0].message.content;
-  } catch (error) {
-    console.log(error);
-    return undefined;
-  }
+
+  const reader = new FileReader()
+  reader.readAsDataURL(imageFile)
+  reader.addEventListener('load', async () => {
+    try {
+      const response = await openai.chat.completions.create({
+        model: "gpt-4-vision-preview",
+        messages: [
+          {
+            role: "user",
+            content: [
+              { type: "text", text: "Що на цьому зображені?" },
+              {
+                type: "image_url",
+                image_url: image_url,
+              },
+            ],
+          },
+        ],
+        max_tokens: 20,
+      });
+      console.log(response.choices[0]);
+      message = response.choices[0].message.content;
+    } catch (error) {
+      console.log(error);
+    }
+    reader.removeEventListener('load')
+  })
+  
   return message;
 }
 
