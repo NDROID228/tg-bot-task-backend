4e66a07924ad03dc57c4bb0fa99d102cec407a74
NDROID228 <djunsonik@gmail.com>
2024-11-13 22:46:38 +0200

    backend

--- index.js
+++ index.js
@@ -42,6 +42,8 @@ const express = require("express");
 const formidable = require("express-formidable");
 const bodyParser = require("body-parser");
 const FileReader = require("filereader");
+const multer = require("multer");
+const upload = multer();
 const app = express();
 const port = 5000;
 
@@ -66,95 +68,103 @@ app.get("/", (req, res) => {
 });
 
 // Route for file upload
-app.post("/upload", async (req, res) => {
-  console.log(req.files);
-  const { queryId } = req.body;
-  console.log("queryId:", queryId, "req.body:", req.body);
-  try {
-    if (!req.files || !req.files.image) {
-      console.log(req.files, req.files.image);
-      return res.status(400).send({
-        message: "No image file uploaded.",
-        ok: false,
-      });
-    }
-
-    const file = req.files.image;
-    const mimetype = file.type.toLowerCase();
-    const extname = file.name.split(".").pop();
-
-    if (!checkFileType(mimetype, extname)) {
-      return res.status(400).send({
-        message:
-          "Хибний формат файлу. Тільки JPEG, JPG, and PNG файли дозволені.",
-        ok: false,
-      });
-    }
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-
-    reader
-      .addEventListener("load", async () => {
-        const promise = analyzeImage(reader.result);
-        promise.then(async (description) => {
-          if (description !== undefined) {
-            console.log("description:", description);
-
-            bot.answerWebAppQuery(queryId, {
-                type: "article",
-                id: queryId,
-                title: "Відповідь Vision",
-                input_message_content: { message_text: description },
-              })
-              .then((description) => {
-                console.log("succesful query: ");
-              })
-              .catch((error) => {
-                console.log(error);
-                bot
-                  .answerWebAppQuery(queryId, {
-                    type: "article",
-                    id: queryId,
-                    title: "Не вділося отримати відповідь Vision",
-                    input_message_content: {
-                      message_text: "Не вділося отримати відповідь Vision",
-                    },
-                  })
-                  .then(() =>
-                    res.status(500).send({
-                      message: "Не вділося отримати відповідь Vision",
-                      ok: false,
-                    })
-                  )
-                  .catch(() =>
-                    res.status(500).send({
-                      message: "Не вділося отримати відповідь Vision",
-                      ok: false,
-                    })
-                  );
-              });
-          } else {
-            res.send({
-              message: `Щось пішло не так... Спробуйте ще.`,
-              ok: false,
-            });
-          }
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send({
-          message: "Під час обробки картинки виникла помилка...",
+app.post(
+  "/upload",
+  upload.fields([
+    { name: "image", maxCount: 1 },
+    { name: "queryId", maxCount: 1 },
+  ]),
+  async (req, res) => {
+    console.log(req.files);
+    const { queryId } = req.body;
+    console.log("queryId:", queryId, "req.body:", req.body);
+    try {
+      if (!req.files || !req.files.image) {
+        console.log(req.files, req.files.image);
+        return res.status(400).send({
+          message: "No image file uploaded.",
           ok: false,
         });
+      }
+
+      const file = req.files.image;
+      const mimetype = file.type.toLowerCase();
+      const extname = file.name.split(".").pop();
+
+      if (!checkFileType(mimetype, extname)) {
+        return res.status(400).send({
+          message:
+            "Хибний формат файлу. Тільки JPEG, JPG, and PNG файли дозволені.",
+          ok: false,
+        });
+      }
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+
+      reader
+        .addEventListener("load", async () => {
+          const promise = analyzeImage(reader.result);
+          promise.then(async (description) => {
+            if (description !== undefined) {
+              console.log("description:", description);
+
+              bot
+                .answerWebAppQuery(queryId, {
+                  type: "article",
+                  id: queryId,
+                  title: "Відповідь Vision",
+                  input_message_content: { message_text: description },
+                })
+                .then((description) => {
+                  console.log("succesful query: ");
+                })
+                .catch((error) => {
+                  console.log(error);
+                  bot
+                    .answerWebAppQuery(queryId, {
+                      type: "article",
+                      id: queryId,
+                      title: "Не вділося отримати відповідь Vision",
+                      input_message_content: {
+                        message_text: "Не вділося отримати відповідь Vision",
+                      },
+                    })
+                    .then(() =>
+                      res.status(500).send({
+                        message: "Не вділося отримати відповідь Vision",
+                        ok: false,
+                      })
+                    )
+                    .catch(() =>
+                      res.status(500).send({
+                        message: "Не вділося отримати відповідь Vision",
+                        ok: false,
+                      })
+                    );
+                });
+            } else {
+              res.send({
+                message: `Щось пішло не так... Спробуйте ще.`,
+                ok: false,
+              });
+            }
+          });
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send({
+            message: "Під час обробки картинки виникла помилка...",
+            ok: false,
+          });
+        });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({
+        message: "Під час обробки картинки виникла помилка...",
+        ok: false,
       });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({
-      message: "Під час обробки картинки виникла помилка...",
-      ok: false,
-    });
+    }
   }
-});
+);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
